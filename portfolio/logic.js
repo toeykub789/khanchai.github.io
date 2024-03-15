f8f53046fd4ff7042b802c337b5b275e8c1b3bd7
khanchai phaetchatturas <143311980+toeykub789@users.noreply.github.com>
2024-03-15 12:27:49 +0700

    Add files via upload

---
+++ portfolio/logic.js
@@ -0,0 +1,20 @@
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute("href").substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    targetElement.scrollIntoView({ behavior: "smooth" });
+  });
+});
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute("href").substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    targetElement.scrollIntoView({ behavior: "smooth" });
+  });
+});
